d733c9252e9982723b7160a6170bedbda0f47b5d
nicha <nichasgarden@gmail.com>
2025-08-20 18:15:03 +0800

    chatbot echoes commands entered by user and exits when user types: bye

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -1,9 +1,30 @@
+import java.util.*;
+
 public class Bug {
     public static void main(String[] args) {
         String name = "Bug";
+        Scanner sc = new Scanner(System.in);
+
+        // greetings
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
+
+        // input and processing input
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+
+            if("bye".equalsIgnoreCase(input)) {
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
+
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
