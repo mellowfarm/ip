ffdb9306cb3c5a80cf2aa50bab31a4598d84c8cf
nicha <nichasgarden@gmail.com>
2025-08-20 17:53:30 +0800

    renamed chatbot, did greeting and exit messages

---
+++ src/main/java/Bug.java
@@ -0,0 +1,9 @@
+public class Bug {
+    public static void main(String[] args) {
+        String name = "Bug";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
