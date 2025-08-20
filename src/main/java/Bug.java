4424043a67a18751215591fdad23723729c0975f
nicha <nichasgarden@gmail.com>
2025-08-20 23:44:39 +0800

    add support for tracking 3 types of tasks

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -15,34 +15,62 @@ public class Bug {
         // input and processing input
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
+            String[] split = input.split("\\s+", 2);
+            String instruction = split[0];
+            String contents = (split.length > 1) ? split[1] : "";
 
-            if ("bye".equalsIgnoreCase(input)) {
+            if ("bye".equals(instruction)) {
                 break;
             }
 
-            if ("list".equalsIgnoreCase(input)) {
+            if ("list".equals(instruction)) {
                 for (int i = 1; i <= size; i++) {
-                    System.out.println(i + ".[" + tasks[i - 1].getStatusIcon() + "] " + tasks[i - 1].getDescription());
+                    System.out.println(i + "." + tasks[i - 1].toString());
                 }
                 System.out.println("____________________________________________________________");
-            } else if ("mark".equalsIgnoreCase(input.trim().split("\\s+", 2)[0])) {
-                int taskNum = Integer.parseInt(input.trim().split("\\s+", 2)[1]) - 1;
+            } else if ("mark".equals(instruction)) {
+                int taskNum = Integer.parseInt(contents) - 1;
                 tasks[taskNum].markAsDone();
                 System.out.println("Nice! I've marked this task as done:\n[" + tasks[taskNum].getStatusIcon() + "] " +
                         tasks[taskNum].getDescription());
                 System.out.println("____________________________________________________________");
-            } else if ("unmark".equalsIgnoreCase(input.trim().split("\\s+", 2)[0])) {
-                int taskNum = Integer.parseInt(input.trim().split("\\s+", 2)[1]) - 1;
+            } else if ("unmark".equals(instruction)) {
+                int taskNum = Integer.parseInt(contents) - 1;
                 tasks[taskNum].markAsUndone();;
                 System.out.println("OK, I've marked this task as not done yet:\n[" + tasks[taskNum].getStatusIcon() + "] " +
                         tasks[taskNum].getDescription());
                 System.out.println("____________________________________________________________");
             } else {
-                tasks[size] = new Task(input);
+                if ("todo".equals(instruction)) {
+                    tasks[size] = new ToDos(contents);
+                    int num = size + 1;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Ok! I've added this task:\n" + tasks[size].toString() +
+                            "\nNow you have " + num + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                } else if ("deadline".equals(instruction)) {
+                    String[] parts = contents.split("/", 2);
+                    String desc = parts[0];
+                    String by = parts[1].trim().split("\\s+", 2)[1];
+                    tasks[size] = new Deadlines(desc, by);
+                    int num = size + 1;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Ok! I've added this task:\n" + tasks[size].toString() +
+                            "\nNow you have " + num + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                } else {
+                    String[] parts = contents.split("/", 3);
+                    String desc = parts[0];
+                    String start = parts[1].trim().split("\\s+", 2)[1];
+                    String end = parts[2].trim().split("\\s+", 2)[1];
+                    tasks[size] = new Events(desc, start, end);
+                    int num = size + 1;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Ok! I've added this task:\n" + tasks[size].toString() +
+                            "\nNow you have " + num + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                }
                 size++;
-                System.out.println("____________________________________________________________");
-                System.out.println("added: " + input);
-                System.out.println("____________________________________________________________");
             }
 
 
