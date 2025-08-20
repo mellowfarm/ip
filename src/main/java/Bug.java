bff83fb4f71b0e0d84975f9ff1dd56cb87951119
nicha <nichasgarden@gmail.com>
2025-08-20 18:57:36 +0800

    added the ability to mark tasks as done and change back to undone

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -4,7 +4,7 @@ public class Bug {
     public static void main(String[] args) {
         String name = "Bug";
         Scanner sc = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int size = 0;
 
         // greetings
@@ -22,16 +22,30 @@ public class Bug {
 
             if ("list".equalsIgnoreCase(input)) {
                 for (int i = 1; i <= size; i++) {
-                    System.out.println(i + ". " + tasks[i - 1]);
+                    System.out.println(i + ".[" + tasks[i - 1].getStatusIcon() + "] " + tasks[i - 1].getDescription());
                 }
                 System.out.println("____________________________________________________________");
+            } else if ("mark".equalsIgnoreCase(input.trim().split("\\s+", 2)[0])) {
+                int taskNum = Integer.parseInt(input.trim().split("\\s+", 2)[1]) - 1;
+                tasks[taskNum].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n[" + tasks[taskNum].getStatusIcon() + "] " +
+                        tasks[taskNum].getDescription());
+                System.out.println("____________________________________________________________");
+            } else if ("unmark".equalsIgnoreCase(input.trim().split("\\s+", 2)[0])) {
+                int taskNum = Integer.parseInt(input.trim().split("\\s+", 2)[1]) - 1;
+                tasks[taskNum].markAsUndone();;
+                System.out.println("OK, I've marked this task as not done yet:\n[" + tasks[taskNum].getStatusIcon() + "] " +
+                        tasks[taskNum].getDescription());
+                System.out.println("____________________________________________________________");
             } else {
-                tasks[size] = input;
+                tasks[size] = new Task(input);
                 size++;
                 System.out.println("____________________________________________________________");
                 System.out.println("added: " + input);
                 System.out.println("____________________________________________________________");
             }
+
+
         }
 
         System.out.println("____________________________________________________________");
